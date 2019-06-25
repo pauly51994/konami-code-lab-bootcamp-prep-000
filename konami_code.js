d995a4e2d3a06ce37cd029535963f4a37bc4352b
Paul Yun <p.yun1994@gmail.com>
2019-06-25 19:43:20 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,17 @@ const codes = [
   "a"
 ];
 
+
 function init() {
-  // your code here
+  document.body.addEventListener('keydown', function(button){
+    let index = 0;
+    let input = button.key;
+    if (input === codes[index]){
+      index++;
+      if(index === codes.length){
+        alert('CONGRATS!');
+        index = 0;
+      }
+    } else {index = 0}
+  })
 }
